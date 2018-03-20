32d52c266e34cc5d8f94a2a7b949ae7c9ae9a07f
chobojai123 <davidcheng1290@gmail.com>
2018-03-19 20:34:51 -0700

    Complete backcast through rendering video player

--- src/views/app.js
+++ src/views/app.js
@@ -3,15 +3,25 @@ var AppView = Backbone.View.extend({
   el: '#app',
 
   initialize: function() {
-    this.videos = new Videos();
+    // console.log(this.collection)
+    this.videos = new Videos(this.collection);
+    this.render();
   },
 
 
   render: function() {
     this.$el.html(this.template());
+    new VideoListView({
+      collection: this.videos 
+    }).render();
+    new VideoPlayerView({
+      collection: this.videos
+    }).render();
     return this;
   },
 
   template: templateURL('src/templates/app.html')
 
 });
+
+
--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,9 +1,20 @@
 var VideoListView = Backbone.View.extend({
-
+  
+  el: '.list',
+  
+  initialize: function() {
+    this.listenTo(this.collection, 'sync', this.render);
+  },
+  
+  // exampleVideoData = this.collection
 
   render: function() {
-    this.$el.children().detach();
     this.$el.html(this.template());
+    this.$el.find('.video-list').empty();
+    this.collection.each(function(video) {
+      var videoView = new VideoListEntryView({'model': video});
+      this.$el.find('.video-list').append(videoView.render().$el);
+    }, this);
     return this;
   },
 
